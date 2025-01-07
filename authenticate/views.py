739bf1bb2ceddb8a5612ed7f7183fd3483a2013d
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-07 16:23:46 +0000

    authenticate.html is not important and Added a message.error in views

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+# Page d'authentification de base
 def auth_view(request):
     return render(request, 'authenticate/authenticate.html', {})
 
-
+# Connexion utilisateur
 def login_user(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
-            return redirect('login')
+            messages.success(request, "You have successfully logged in.")
+            return redirect('home')  
         else:
-            return redirect('products')
+            messages.error(request, "Invalid username or password.")
+            return redirect('login')  
     else:
         return render(request, 'authenticate/login.html', {})
+
+# Déconnexion utilisateur
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out.")
+    return redirect('login')  
