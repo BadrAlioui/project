008b33373bb277a346e5d86cbaed91186c778ecb
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-08 17:59:34 +0000

    Added class from models. py into admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product, Order
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'brand', 'price', 'rating', 'image']
+    
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Order)
+
+    
