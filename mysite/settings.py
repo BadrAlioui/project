ef750bfa4868dbad88f04281b6f1225235283330
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-08 07:36:44 +0000

    Added name in register

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,9 @@ import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -143,6 +146,13 @@ DEFAULT_FROM_EMAIL = 'studentinstitute2024@gmail.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+cloudinary.config(
+    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
+    api_key=config('CLOUDINARY_API_KEY'),
+    api_secret=config('CLOUDINARY_API_SECRET')
+)
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- store/models.py
+++ store/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True, default="default-slug")
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=250)
+    brand = models.CharField(max_length=250, default='un-branded')
+    description = models.TextField()
+    slug = models.SlugField(max_length=250, unique=True, default="default-slug")
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                validators=[MinValueValidator(0.1)])
+    image = CloudinaryField('image', null=True, blank=True)
+    stock = models.IntegerField(default=0, blank=True, verbose_name="Stock du produit")
+
+    class Meta:
+        verbose_name_plural = "products"
+
+    def __str__(self):
+        return self.title
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.TextField()
+    reference = models.CharField(max_length=128)
+    date = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=255, default="Created")
+    price = models.FloatField(default=0.0, blank=True)
+    
+
+    def __str__(self):
+        return f"Order by {self.user.username} - Status: {self.status}"
+
+    
